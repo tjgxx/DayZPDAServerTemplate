59a95b5303e423dc7dc861ad79d4087bcfa8ff81
Taylor Griffin <taylorjosephgriffin@gmail.com>
2024-12-15 14:26:30 -0800

    'cors'

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-Requested-With");
+
+  next();
+});
+
 // Database Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
